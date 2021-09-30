c88ebd7c365ffe9c4369a346c93d4fa9e541e24e
lieven clement <lclement@lievens-MacBook-Pro.local>
2021-09-30 15:58:55 +0200

    binder commit

---
+++ README.md
@@ -0,0 +1,6 @@
+#  SGA21 - Statistical Genomics 2021
+
+[Binder](https://mybinder.org/) instance to run Rstudio with the required packages for this course in the cloud. 
+
+
+[![Binder](http://mybinder.org/badge.svg)](https://mybinder.org/v2/gh/statOmics/SGA21/binder?urlpath=rstudio)
---
+++ install.R
@@ -0,0 +1,28 @@
+## Add packages used in Rmd files
+
+pkgs <- c(
+  'remotes',
+  'devtools',
+  'tidyverse',
+  'latex2exp',
+  'plotly',
+  'rmarkdown',
+  'knitr',
+  'car',
+  'multcomp',
+  'gridExtra',
+  'ExploreModelMatrix',
+  'plot3D',
+  'ggplot2',
+  'MASS',
+  'SummarizedExperiment',
+  'parathyroidSE',
+  'edgeR',
+  'limma',
+  'DESeq2',
+  'scales',
+  'QFeatures',
+  'msqrob2')
+
+install.packages("BiocManager", Ncpus = 2L)
+BiocManager::install(pkgs)
